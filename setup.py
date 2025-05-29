a9840a374862c2ecd4cea89f5461a41b3a5e8bcd
wrightky <kyleawright@utexas.edu>
2025-05-29 17:03:22 -0500

    installation files

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    LONG_DESCRIPTION = f.read()
+
+setup(
+    name = 'coastx',
+    version = '1.0.0',
+    license = 'MIT',
+    description = 'Linear Reference Frame for the Coast of Texas',
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type = 'text/markdown',
+    author = 'Kyle Wright',
+    author_email = 'Kyle.Wright@twdb.texas.gov',
+    url = 'https://github.com/wrightky/CoastalTXProj',
+    packages = find_packages(),
+    keywords='Texas coastal geotransform reference frame',
+    classifiers = [
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Scientific/Engineering :: GIS'
+    ],
+    install_requires = ['numpy','matplotlib','xarray'],
+)
